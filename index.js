ae06c21103415790580780b296bbd852774d6eec
Eric McCarthy <eric@limulus.net>
2013-04-30 23:49:54 -0700

    Successfully creates objects from files.

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 "use strict"
 
+var fs = require('fs')
+  , path = require('path')
+
 /**
  * @constuctor
+ * @param {string} dirPath
+ * @param {function(string, string, function(*))} objectCreator
  */
-var Mache = module.exports = function (path) {
-	this._path = path
+var Mache = module.exports = function (dirPath, objectCreator) {
+	this._dirPath = dirPath
+    this._objectCreator = objectCreator
 }
 
 /**
  * @return {string}
  */
 Mache.prototype.path = function () {
-	return this._path
+	return this._dirPath
 }
 
 /**
+ * @param {string} file
+ * @param {function(Error?, *)} returnResult
+ */
+Mache.prototype.get = function (file, returnResult) {
+    var fullFilePath = path.join(this.path(), file)
+    fs.readFile(fullFilePath, function (err, data) {
+        var objectCreator = this._objectCreator
+
+        if (err) {
+            returnResult(err)
+        }
+        else {
+            objectCreator(file, data, function (obj) {
+                // ... add obj to cache
+                returnResult(undefined, obj)
+            })
+        }
+    }.bind(this))
+}
+
+/**
+ * @param {string} dirPath
+ * @param {function(string, string, function(*))} objectCreator
  * @return {Mache}
  */
-Mache.create = function (path) {
-	return new Mache(path)
+Mache.create = function (dirPath, objectCreator) {
+	return new Mache(dirPath, objectCreator)
 }
--- tests/mache_test.js
+++ tests/mache_test.js
@@ -2,21 +2,36 @@
 
 var mache = require('../index.js')
   , assert = require('assert')
-  , Tempdir = require('temporary/lib/dir')
+  , temp = require('temp')
+  , fs = require('fs')
+  , path = require('path')
 
-var testDir = new Tempdir
-process.on('exit', function () {
-	testDir.rmdirSync()
+var testDir = temp.mkdirSync()
+
+var testData1 = '{"id": 1}'
+fs.writeFileSync(path.join(testDir, '1.json'), testData1)
+
+var testDataCache
+beforeEach(function () {
+	testDataCache = mache.create(testDir, function (path, data, macheUpdate) {
+		var testObj = JSON.parse(data)
+		testObj._path = path
+		macheUpdate(testObj)
+	})
 })
 
 describe('mache.create', function () {
-	var testDataCache
-
-	beforeEach(function () {
-		testDataCache = mache.create(testDir.path)
-	})
-
 	it('should create a new Mache object with the given path', function () {
-		assert.strictEqual(testDataCache.path(), testDir.path)
+		assert.strictEqual(testDataCache.path(), testDir)
+	})
+})
+
+describe('#get', function () {
+	it('should return the correct object for the given file', function (done) {
+		testDataCache.get('1.json', function (err, testObj1) {
+			if (err) throw err
+			assert.strictEqual(testObj1.id, 1)
+			done()
+		})
 	})
 })
